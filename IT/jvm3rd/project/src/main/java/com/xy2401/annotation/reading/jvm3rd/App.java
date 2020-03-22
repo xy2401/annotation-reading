06228d3cf5f1536b8b27f05ffa181d30cc709670
xy2401 <xy2401@163.com>
2020-03-22 23:36:34 +0800

    重命名 && 附件提交

---
+++ IT/jvm3rd/project/src/main/java/com/xy2401/annotation/reading/jvm3rd/App.java
@@ -0,0 +1,13 @@
+package com.xy2401.annotation.reading.jvm3rd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
